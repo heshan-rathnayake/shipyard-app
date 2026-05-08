641ab6fc0e5d8972a871b6b1dde629900c6d65a7
just4heshan <heshanravindussc@gmail.com>
2026-05-08 23:26:01 +1000

    feat(app-router): add project, task, and comment routers to main app router

--- packages/api/src/server/routers/_app.ts
+++ packages/api/src/server/routers/_app.ts
@@ -2,11 +2,17 @@ import { router } from "../trpc";
 import { organizationRouter } from "./organization";
 import { memberRouter } from "./member";
 import { activityLogRouter } from "./activityLog";
+import { projectRouter } from "./project";
+import { taskRouter } from "./task";
+import { commentRouter } from "./comment";
 
 export const appRouter = router({
   organization: organizationRouter,
   member: memberRouter,
   activityLog: activityLogRouter,
+  project: projectRouter,
+  task: taskRouter,
+  comment: commentRouter,
 });
 
 export type AppRouter = typeof appRouter;
